3707d94d71e5fbebb91f323e5da2abbf6d28b753
ST10463096 <ST10463096@rcconnect.edu.za>
2026-08-06 14:18:36 +0200

    Update Program.cs

--- Chatbot/Chatbot/Program.cs
+++ Chatbot/Chatbot/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CybersecurityChatbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cybersecurity Awareness Bot";
+
+            Console.SetWindowSize(120, 40);
+
+            Chatbot chatbot = new Chatbot();
+
+            chatbot.Start();
+
+            Console.WriteLine("\n\nPress any Key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
