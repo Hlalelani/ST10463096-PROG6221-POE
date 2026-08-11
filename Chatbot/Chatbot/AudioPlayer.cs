c8b73a585154d6a4171a26e305c8007b0227f9a4
ST10463096 <ST10463096@rcconnect.edu.za>
2026-08-11 16:08:32 +0200

    Create AudioPlayer.cs

---
+++ Chatbot/Chatbot/AudioPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Media;
+using System.IO;
+
+namespace CybersecurityChatbot
+{
+    public class AudioPlayer
+    {
+        private string audioFilePath;
+
+        public AudioPlayer()
+        {
+            audioFilePath = FindAudioFile();
+        }
+        private string FindAudioFile()
+        {
+         string[] possiblePaths =
+                { Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "greeting.wav"), 
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, " ..", "..", "..", "greeting.wav"),
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "greeting.wav"), "greeting.wav" };
+
+            foreach (string Path in possiblePaths)
+            {
+                if (File.Exists(Path))
+                {
+                    return Path;
+                }
+            }
+            return "greeting.wav ";
+        }
+
+        public void PlayGreeting()
+        {
+            try
+            {
+                if (!File.Exists(audioFilePath))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Audio file not found. voice greeting skipped.");
+
+                    Console.ResetColor();
+
+                    return;
+                }
+
+                using (SoundPlayer player =new SoundPlayer(audioFilePath))
+                {
+                    player.Play();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Playing voice greeting...");
+                    Console.ResetColor();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Could not play audio : {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        
+
+    }
+}
