1869e1eb42c46773d103a3f6189a50c1bb2024eb
ST10463096 <ST10463096@rcconnect.edu.za>
2026-08-14 15:18:19 +0200

    Update Program.cs

--- Chatbot/Chatbot/Program.cs
+++ Chatbot/Chatbot/Program.cs
@@ -6,13 +6,16 @@ namespace CybersecurityChatbot
     {
         static void Main(string[] args)
         {
+#pragma warning disable CA1416
+
             Console.Title = "Cybersecurity Awareness Bot";
 
             Console.SetWindowSize(120, 40);
+#pragma warning restore CA1416
 
             Chatbot chatbot = new Chatbot();
 
-            chatbot.start();
+            chatbot.Start();
 
             Console.WriteLine("\n\nPress any Key to exit...");
             Console.ReadKey();
