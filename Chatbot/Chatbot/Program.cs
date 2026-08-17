d36b3073b793955b690eb6a38c265681751d0e6a
ST10463096 <ST10463096@rcconnect.edu.za>
2026-08-17 12:46:49 +0200

    Update Program.cs

--- Chatbot/Chatbot/Program.cs
+++ Chatbot/Chatbot/Program.cs
@@ -7,17 +7,21 @@ namespace CybersecurityChatbot
         static void Main(string[] args)
         {
 #pragma warning disable CA1416
-
-            Console.Title = "Cybersecurity Awareness Bot";
-
-            Console.SetWindowSize(120, 40);
+            Console.Title = " Cybersecurity Awareness Assistant";
+            Console.SetWindowSize(120, 45);
 #pragma warning restore CA1416
 
-            Chatbot chatbot = new Chatbot();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("╔══════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║   Starting Cybersecurity Awareness Assistant...                                      ║");
+            Console.WriteLine("╚══════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+            Console.WriteLine();
 
+            Chatbot chatbot = new Chatbot();
             chatbot.Start();
 
-            Console.WriteLine("\n\nPress any Key to exit...");
+            Console.WriteLine("\n\nPress any key to exit...");
             Console.ReadKey();
         }
     }
