c970d7253537853418d02e3532bf47d01080d6c7
ST10463096 <ST10463096@rcconnect.edu.za>
2026-08-17 14:19:27 +0200

    Update Chatbot.AssemblyInfo.cs

--- Chatbot/Chatbot/obj/Debug/net9.0/Chatbot.AssemblyInfo.cs
+++ Chatbot/Chatbot/obj/Debug/net9.0/Chatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a47616141da9d8ff3b5b1d4c099bdfa62952deaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee6258a343844cadaf0db5a8c0559a733712f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
